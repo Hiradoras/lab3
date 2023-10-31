473808e254f77d9a71130d5ac5de52dfd06f7e88
Hiradoras <forwoermind@gmail.com>
2023-10-31 15:52:42 +0600

    Add project files.

---
+++ lab3/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            groupBox1 = new GroupBox();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            button2 = new Button();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(11, 15);
+            label1.TabIndex = 0;
+            label1.Text = "(";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(29, 6);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(107, 55);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Result";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(56, 135);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 100);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "askjfdskljd";
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(79, 75);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(37, 19);
+            radioButton3.TabIndex = 12;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "50";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(79, 50);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(37, 19);
+            radioButton2.TabIndex = 11;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "20";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(79, 25);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(37, 19);
+            radioButton1.TabIndex = 10;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "10";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(135, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(15, 15);
+            label2.TabIndex = 4;
+            label2.Text = "+";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(156, 9);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(262, 12);
+            label3.Name = "label3";
+            label3.Size = new Size(11, 15);
+            label3.TabIndex = 6;
+            label3.Text = ")";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(279, 12);
+            label4.Name = "label4";
+            label4.Size = new Size(12, 15);
+            label4.TabIndex = 7;
+            label4.Text = "/";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(70, 96);
+            label5.Name = "label5";
+            label5.Size = new Size(32, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Total";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(297, 12);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 9;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(108, 256);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 10;
+            button2.Text = "Result";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(108, 93);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 12;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(70, 309);
+            label6.Name = "label6";
+            label6.Size = new Size(32, 15);
+            label6.TabIndex = 11;
+            label6.Text = "Total";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(108, 306);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(425, 379);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(label6);
+            Controls.Add(button2);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(groupBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private GroupBox groupBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox3;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button2;
+        private TextBox textBox4;
+        private Label label6;
+        private TextBox textBox5;
+    }
+}
